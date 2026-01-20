cc45f501e2c455141c64912e8b73347b8c7d01e7
codexmish <majharulislam.shanto.70@gmail.com>
2026-01-20 11:47:54 +0600

    IDE header done

--- src/assets/constants/index.js
+++ src/assets/constants/index.js
@@ -1,16 +1,31 @@
 const menuItem = [
-    {
-        itemName: "Features",
-        itemLink: "#Features"
-    },
-    {
-        itemName: "Pricing",
-        itemLink: "#pricing"
-    },
-    {
-        itemName: "Testimonials",
-        itemLink: "#testimonials"
-    },
+  {
+    itemName: "Features",
+    itemLink: "#Features",
+  },
+  {
+    itemName: "Pricing",
+    itemLink: "#pricing",
+  },
+  {
+    itemName: "Testimonials",
+    itemLink: "#testimonials",
+  },
 ];
 
-export { menuItem };
+const dotColors = [
+  {
+    colorName: "Red",
+    colorCode: "bg-red-500",
+  },
+  {
+    colorName: "Yellow",
+    colorCode: "bg-yellow-500",
+  },
+  {
+    colorName: "Green",
+    colorCode: "bg-green-500",
+  },
+];
+
+export { menuItem, dotColors };
