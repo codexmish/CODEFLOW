3fa131d3c6ce9a9c8c95a1dd11bb4382578c0f63
codexmish <majharulislam.shanto.70@gmail.com>
2026-01-21 11:39:02 +0600

    banner added

--- src/components/Banner.jsx
+++ src/components/Banner.jsx
@@ -5,7 +5,7 @@ import {
   fileTabs,
   floatingCards,
 } from "../assets/constants";
-import { ChevronDown, Sparkles } from "lucide-react";
+import { ArrowRight, ChevronDown, Play, Sparkles } from "lucide-react";
 import SyntaxHighlighter from "react-syntax-highlighter";
 import { nightOwl } from "react-syntax-highlighter/dist/esm/styles/hljs";
 
@@ -40,125 +40,135 @@ const Banner = () => {
         {/* ------bottom right bg color animation */}
         <div className="absolute bottom-20 right-4 sm:right-10 w-64 sm:w-96 h-64 sm:h-96 bg-cyan-500/10 rounded-full blur-3xl animate-pulse delay-1000"></div>
 
-
-        <div className="max-w-7xl mx-auto text-center relative w-full">
-          <div className="max-w-7xl mx-auto flex flex-col lg:grid lg:grid-cols-2 gap-6 sm:gap-8 lg:gap-12 items-center relative">
-           {/* -------left content */}
+        <div className="max-w-7xl px-4 md:px-0 mx-4 md:mx-auto text-center relative w-full">
+          <div className="max-w-7xl mx-auto flex flex-col lg:grid lg:grid-cols-2 text-center lg:text-left gap-6 sm:gap-8 lg:gap-12 items-center relative">
+            {/* -------left content */}
             <div>
               <div className="inline-flex items-center gap-x-2 px-3 sm:px-4 py-2 bg-blue-500/10 border border-blue-500/20 rounded-full mb-4 sm:mb-6 animate-in slide-in-from-bottom duration-700">
-
-              <Sparkles className="w-4 h-4 text-blue-400"/>
-              <span className="text-xs sm:text-sm text-blue-300">Introducing Codeflow AI</span>
-              </div>
-
-              <h1 className="text-5xl sm:text-3xl md:text-4xl lg:text-5xl font-semibold mb-4 sm:mb-6 animate-in slide-in-from-bottom duration-700 delay-100">
-                <span>Code Faster</span>
-                <span>Build Better</span>
-                <span>With CodeFlow AI</span>
-              </h1>
-
-
-
-            </div>
-
-         {/* ------right content */}
-        <div className="relative order-2 w-full">
-          <div className="relative bg-white/5 backdrop-blur-xl rounded-xl sm:rounded-2xl p-3 sm:p-4 shadow-2xl border border-white/10">
-            <div className="bg-gradient-to-br from-gray-900/20 to-gray-800/20 backdrop-blur-sm rounded-lg overflow-hidden h-[280px] sm:w-[350px] lg:h-[450px] border-white/5">
-              {/* -----IDE Header */}
-              <div className="flex items-center justify-between px-3  sm:px-4 py-2 sm:py-3 bg-white/5 backdrop-blur-sm border-b border-white/10">
-                <div className="flex items-center gap-x-2">
-                  <div className="dots flex items-center gap-x-1 sm:gap-x-2">
-                    {dotColors.map((item, key) => (
-                      <div
-                        key={key}
-                        className={`w-2 sm:w-3 h-2 sm:h-3 rounded-full ${item.colorCode}`}
-                      ></div>
-                    ))}
-                  </div>
-
-                  <span className="text-xs sm:text-sm text-gray-300">
-                    CodeFlow AI
-                  </span>
-                </div>
-                <ChevronDown className="w-3 sm:w-4 h-3 sm:h-4 text-gray-400" />
-              </div>
-              {/* -------code body */}
-              <div className="p-3 sm:p-4 relative h-full">
-                {/* -----file tabs */}
-                <div className="flex gap-x-1 sm:gap-x-2 mb-3 sm:mb-4 overflow-x-auto">
-                  {fileTabs.map((item, key) => (
-                    <button
-                      onClick={() => setActiveTab(item.tabName)}
-                      key={key}
-                      className={`px-3 py-2 backdrop-blur-sm text-xs sm:text-sm rounded-t-lg border ${activeTab === item.tabName ? "bg-blue-500/30 text-white border-blue-400/20" : "bg-white/5 text-gray-300 border-white/10 hover:bg-white/10"} transition-all duration-200 whitespace-nowrap`}
-                    >
-                      {item.tabName}
-                    </button>
-                  ))}
-                </div>
-
-                {/* ----code content */}
-                <div className="relative overflow-hidden flex-grow">
-                  <SyntaxHighlighter
-                    language="javascript"
-                    style={nightOwl}
-                    customStyle={{
-                      margin: 0,
-                      borderRadius: "8px",
-                      fontSize: "11px",
-                      lineHeight: "1.4",
-                      height: "100%",
-                      border: "1px solid #3c3c3c",
-                    }}
-                  >
-                    {codeExamples[activeTab]}
-                  </SyntaxHighlighter>
-                </div>
-              </div>
-            </div>
-
-            {/* ---------floating cards */}
-            <div
-              className={`hidden lg:block absolute bottom-4 right-4 transform translate-x-8 translate-y-8 w-72 ${currentFloatingCard.bgColor} backdrop-blur-xl rounded-lg p-4 border border-white/20 shadow-2xl`}
-            >
-              <div className="flex items-center gap-x-2 mb-2">
-                <div
-                  className={`w-6 h-6 ${currentFloatingCard.iconColor} flex items-center justify-center text-sm font-bold`}
-                >
-                  {currentFloatingCard.icon}
-                </div>
-                <span
-                  className={`text-sm font-medium${currentFloatingCard.textColor}`}
-                >
-                  {currentFloatingCard.title}
+                <Sparkles className="w-4 h-4 text-blue-400" />
+                <span className="text-xs sm:text-sm text-blue-300">
+                  Introducing Codeflow AI
                 </span>
               </div>
 
-              <div
-                className={`text-sm text-left ${currentFloatingCard.contentColor}`}
-              >
-                {currentFloatingCard.content}
+              <h1 className="text-5xl sm:text-3xl md:text-4xl lg:text-5xl xl:text-6xl font-semibold mb-4 sm:mb-6 animate-in slide-in-from-bottom duration-700 delay-100 leading-tight">
+                <span className="bg-gradient-to-r from-white via-blue-100 to-cyan-100 bg-clip-text text-transparent block mb-1 sm:mb-2">
+                  Code Faster
+                </span>
+                <span className="bg-gradient-to-r from-blue-400 via-cyan-400 to-blue-400 bg-clip-text text-transparent block mb-1 sm:mb-2">
+                  Build Better
+                </span>
+                <span className="bg-gradient-to-r from-white via-blue-100 to-cyan-100 bg-clip-text text-transparent block mb-1 sm:mb-2">
+                  With CodeFlow AI
+                </span>
+              </h1>
+
+              <p className="text-md sm:text-base lg:text-lg text-gray-400 max-w-2xl mx-auto lg:mx-0 mb-6 sm:mb-8 animate-in slide-in-from-bottom duration-700 delay-200 leading-relaxed">
+                Accelerate your development workflow with intelligent code
+                completion, automated tasting, and smart debugging. Ship
+                production-ready code 10x faster.
+              </p>
+
+              {/* -------buttons */}
+              <div className="flex flex-col sm:flex-row items-center justify-center lg:justify-start gap-3 sm:gap-4 mb-8 sm:mb-12 animate-in slide-in-from-bottom duration-700 delay-300">
+                <button className="group w-full sm:w-auto px-6 sm:px-8 py-3 sm:py-3 bg-gradient-to-b from-blue-600 to-blue-400 rounded-lg font-semibold sm:text-base transition-all duration-300 hover:scale-102 flex items-center justify-center gap-x-2">
+                  Start Coding Free
+                  <ArrowRight className="w-4 sm:w-5 h-4 sm:h-5 group-hover:translate-x-1 transition-transform duration-300" />
+                </button>
+
+                <button className="group w-full sm:w-auto px-6 sm:px-8 py-3 sm:py-3 bg-white/5 backdrop-blur-sm border border-white/10 rounded-lg font-semibold sm:text-base transition-all duration-300 hover:bg-white/10 flex items-center justify-center gap-x-2">
+                  <div className="p-2 bg-white/10 rounded-full group-hover:bg-white/20 duration-300 transition-colors">
+                    <Play className="w-4 sm:w-5 h-4 sm:h-5 fill-white" />
+                  </div>
+                  Watch Demo
+                </button>
+              </div>
+            </div>
+
+            {/* ------right content */}
+            <div className="relative order-2 w-full">
+              <div className="relative bg-white/5 backdrop-blur-xl rounded-xl sm:rounded-2xl p-3 sm:p-4 shadow-2xl border border-white/10">
+                <div className="bg-gradient-to-br from-gray-900/20 to-gray-800/20 backdrop-blur-sm rounded-lg overflow-hidden h-[280px] sm:h-[350px] lg:h-[450px] border-white/5">
+                  {/* -----IDE Header */}
+                  <div className="flex items-center justify-between px-3  sm:px-4 py-2 sm:py-3 bg-white/5 backdrop-blur-sm border-b border-white/10">
+                    <div className="flex items-center gap-x-2">
+                      <div className="dots flex items-center gap-x-1 sm:gap-x-2">
+                        {dotColors.map((item, key) => (
+                          <div
+                            key={key}
+                            className={`w-2 sm:w-3 h-2 sm:h-3 rounded-full ${item.colorCode}`}
+                          ></div>
+                        ))}
+                      </div>
+
+                      <span className="text-xs sm:text-sm text-gray-300">
+                        CodeFlow AI
+                      </span>
+                    </div>
+                    <ChevronDown className="w-3 sm:w-4 h-3 sm:h-4 text-gray-400" />
+                  </div>
+                  {/* -------code body */}
+                  <div className="p-3 sm:p-4 relative h-full">
+                    {/* -----file tabs */}
+                    <div className="flex gap-x-1 sm:gap-x-2 mb-3 sm:mb-4 overflow-x-auto">
+                      {fileTabs.map((item, key) => (
+                        <button
+                          onClick={() => setActiveTab(item.tabName)}
+                          key={key}
+                          className={`px-3 py-2 backdrop-blur-sm text-xs sm:text-sm rounded-t-lg border ${activeTab === item.tabName ? "bg-blue-500/30 text-white border-blue-400/20" : "bg-white/5 text-gray-300 border-white/10 hover:bg-white/10"} transition-all duration-200 whitespace-nowrap`}
+                        >
+                          {item.tabName}
+                        </button>
+                      ))}
+                    </div>
+
+                    {/* ----code content */}
+                    <div className="relative overflow-hidden flex-grow">
+                      <SyntaxHighlighter
+                        language="javascript"
+                        style={nightOwl}
+                        customStyle={{
+                          margin: 0,
+                          borderRadius: "8px",
+                          fontSize: "11px",
+                          lineHeight: "1.4",
+                          height: "100%",
+                          border: "1px solid #3c3c3c",
+                        }}
+                      >
+                        {codeExamples[activeTab]}
+                      </SyntaxHighlighter>
+                    </div>
+                  </div>
+                </div>
+
+                {/* ---------floating cards */}
+                <div
+                  className={`hidden lg:block absolute bottom-4 right-4 transform translate-x-8 translate-y-8 w-72 ${currentFloatingCard.bgColor} backdrop-blur-xl rounded-lg p-4 border border-white/20 shadow-2xl`}
+                >
+                  <div className="flex items-center gap-x-2 mb-2">
+                    <div
+                      className={`w-6 h-6 ${currentFloatingCard.iconColor} flex items-center justify-center text-sm font-bold`}
+                    >
+                      {currentFloatingCard.icon}
+                    </div>
+                    <span
+                      className={`text-sm font-medium${currentFloatingCard.textColor}`}
+                    >
+                      {currentFloatingCard.title}
+                    </span>
+                  </div>
+
+                  <div
+                    className={`text-sm text-left ${currentFloatingCard.contentColor}`}
+                  >
+                    {currentFloatingCard.content}
+                  </div>
+                </div>
               </div>
             </div>
           </div>
         </div>
-          </div>
-        </div>
-
-
-
-
-
-
-
-
-
-
-
-
-
-
       </section>
     </>
   );
