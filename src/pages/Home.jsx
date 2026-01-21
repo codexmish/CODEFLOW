622f41453389600bca7e09c66357a7e6fc054562
codexmish <majharulislam.shanto.70@gmail.com>
2026-01-21 12:02:56 +0600

    feature section data input

--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -1,11 +1,13 @@
 import React from "react";
 import Banner from "../components/Banner";
+import Features from "../components/Features";
 
 const Home = () => {
   return (
     <>
       <div className="px-4 md:px-0">
         <Banner />
+        <Features/>
       </div>
     </>
   );
